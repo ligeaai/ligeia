9ef81f55032258f91797827f2660059ea67f0d46
beisekin <mr.batkaval@gmail.com>
2021-11-12 19:01:16 +0600

    models0005 : updates

--- core/models/_base_domain.py
+++ core/models/_base_domain.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 import datetime
 from datetime import datetime
 import uuid
@@ -14,17 +15,17 @@ class Base_domain(models.Model):
    # x= models.DecimalField(db_column='x', max_digits=12, decimal_places=8,blank=True)
    # y= models.DecimalField(db_column='y', max_digits=12, decimal_places=8,blank=True)
 
-   start_datetime = models.DateTimeField(db_column='start_datetime', auto_now_add=False, blank=False) 
+   start_datetime = models.DateTimeField(db_column='start_datetime', auto_now_add=True, blank=True) 
    end_datetime = models.DateTimeField(db_column='end_datetime', default=end_datetime, blank=False)
    name = models.CharField(db_column='name', max_length=30, blank=False, unique=True)
    short_name = models.CharField(db_column='short_name', max_length=30, blank=True, unique=True)   
    active = models.BooleanField(db_column='active',blank=False, default=True)   
-   operated = models.BooleanField(db_column='operated',default=True)
-   last_updt_user = models.CharField(db_column='last_updt_user', max_length=15, blank=True)
-   last_updt_date = models.DateTimeField(auto_now=True, blank=True, null=True, unique=True)   
+   operated = models.BooleanField(db_column='operated', default=True)
+   last_updt_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, editable = False)
+   last_updt_date = models.DateTimeField(auto_now=True, blank=True, null=True, unique=True, editable = False)   
    row_id = models.UUIDField(db_column='row_id', primary_key = False, default = uuid.uuid4, editable = False)
-   update_source = models.CharField(db_column='update_source', max_length=15, blank=True)
-   version = models.CharField(db_column='version', max_length=15, blank=True)
+   update_source = models.CharField(db_column='update_source', max_length=15, blank=True, editable = False)
+   version = models.CharField(db_column='version', max_length=15, blank=True, editable = False)
    
    # def __str__(self):
    #    if self.name:
