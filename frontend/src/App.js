fffe608f7d2ec9f9a556b3a5b56c2f0d7cb622f6
Artur Kim <augustpark0@gmail.com>
2021-11-22 06:38:44 -0800

    error fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 
 import { ThemeProvider } from '@mui/material/styles';
 import { StyledEngineProvider } from '@mui/material';
+
 import { useSelector } from 'react-redux';
 import Routes from './routes/index';
 import themes from './themes';
 import NavigationScroll from './layouts/NavigationScroll';
+
+
 
 function App() {
     const customization = useSelector((state) => state.customization);
