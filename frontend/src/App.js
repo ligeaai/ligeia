2966bbf7723302bf04ca4850f8ea39474dba2392
Artur Kim <augustpark0@gmail.com>
2021-11-22 07:52:20 -0800

    Merge branch 'main' of https://github.com/beisekin/IEFPP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,22 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from 'react';
-import './App.css';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { StyledEngineProvider } from '@mui/material';
+import { useSelector } from 'react-redux';
+import Routes from './routes/index';
+import themes from './themes';
+import NavigationScroll from './layouts/NavigationScroll';
 
 function App() {
+  const customization = useSelector((state) => state.customization);
   return (
-    <div className="App">
-      <h1>Login</h1>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={themes(customization)}>
+        <NavigationScroll>
+          <Routes />
+        </NavigationScroll>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
-}
-=======
-
-import { ThemeProvider } from '@mui/material/styles';
-import { StyledEngineProvider } from '@mui/material';
-
-import { useSelector } from 'react-redux';
-import Routes from './routes/index';
-import themes from './themes';
-import NavigationScroll from './layouts/NavigationScroll';
-
-
-
-function App() {
-=======
-
-import { ThemeProvider } from '@mui/material/styles';
-import { StyledEngineProvider } from '@mui/material';
-import { useSelector } from 'react-redux';
-import Routes from './routes/index';
-import themes from './themes';
-import NavigationScroll from './layouts/NavigationScroll';
-
-function App() {
->>>>>>> bf3cee6279744b3ef768fe9e27159bed5cf0d9e0
-    const customization = useSelector((state) => state.customization);
-    return (
-        <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={themes(customization)}>
-                <NavigationScroll>
-                    <Routes />
-                </NavigationScroll>
-            </ThemeProvider>
-        </StyledEngineProvider>
-    );
 };
-
-<<<<<<< HEAD
->>>>>>> bf3cee6279744b3ef768fe9e27159bed5cf0d9e0
-=======
->>>>>>> bf3cee6279744b3ef768fe9e27159bed5cf0d9e0
 
 export default App;
