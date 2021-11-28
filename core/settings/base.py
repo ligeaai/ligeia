be0be9ad8c56e4dffcb01b789c3887dfdc228d8f
beisekin <mr.batkaval@gmail.com>
2021-11-29 01:11:54 +0600

    init0015 : requirements update

--- core/settings/base.py
+++ core/settings/base.py
@@ -28,17 +28,17 @@ ALLOWED_HOSTS = ['127.0.0.1',
                  '20.0.0.20',
                  '*']
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
+    'http://0.0.0.0:8000'
 )
 
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-    'http://0.0.0.0:8000'
-]                 
+# CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:8000',
+# ]                 
 
 # Application definition
 
@@ -96,13 +96,17 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'core.urls'
 
-REST_FRAMEWORK = {
+REST_FRAMEWORK = {    
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-                                    # 'knox.auth.TokenAuthentication',
-                                    'rest_framework.authentication.TokenAuthentication',
-                                    'rest_framework.authentication.BasicAuthentication',
-                                    #    'rest_framework.authentication.SessionAuthentication',
-                                      ],
+        # 'knox.auth.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        #    'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
 TEMPLATES = [
