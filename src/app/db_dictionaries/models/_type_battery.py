c9394260ebeffe3f7ac2b9786ca6f7196eae4f62
beisekin <mr.batkaval@gmail.com>
2021-12-06 20:14:23 +0600

    django0009 : dic updates

--- src/app/db_dictionaries/models/_type_battery.py
+++ src/app/db_dictionaries/models/_type_battery.py
@@ -1,5 +1,4 @@
 from django.db import models
-from djongo import models
 import uuid
 from django.conf import settings
 from mptt.models import MPTTModel, TreeForeignKey
@@ -9,9 +8,8 @@ class Type_battery(MPTTModel):
     code = models.CharField(db_column='code', max_length=100, blank=False, unique=True, verbose_name='Code')
     code_text = models.CharField(db_column='code_text', max_length=100, blank=False, unique=True, verbose_name='Code Name')
     
-    parent = models.ForeignKey('self', on_delete = models.CASCADE, null = True, blank=True,)
-    # parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE, verbose_name='Parent')
-    # address = models.EmbeddedField(model_container=Type_battery)
+    # parent = models.ForeignKey('self', on_delete = models.CASCADE, null = True, blank=True,)
+    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE, verbose_name='Parent')
     
     last_updt_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, editable=False, null=True, verbose_name='Last Update User')
     last_updt_date = models.DateTimeField(auto_now=True, blank=True, editable=False, null=True, verbose_name='Last Update Date')
@@ -19,8 +17,6 @@ class Type_battery(MPTTModel):
     create_source = models.CharField(db_column='create_source', max_length=100, blank=True, editable=False, null=True, verbose_name='Create Source')
     update_source = models.CharField(db_column='update_source', max_length=100, blank=True, editable=False, null=True, verbose_name='Update Source')
     version = models.CharField(db_column='version', max_length=100, blank=True, editable=False, null=True, verbose_name='Version')
-
-    # objects = models.DjongoManager()
 
     def __str__(self):
         if self.code_text:
