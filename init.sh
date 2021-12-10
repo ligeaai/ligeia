06483056eab63bb99193531c49c750d35a47d7ed
beisekin <mr.batkaval@gmail.com>
2021-12-10 08:27:23 +0600

    init0037 : docker updates

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Running collectstatic"
-python backend/manage.py collectstatic --noinput
-echo "Running migrations"
-python backend/manage.py migrate --noinput
-
-/usr/bin/supervisord
