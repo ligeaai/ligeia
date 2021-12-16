f42c607f25cc358f325eb789f54c5b7b5b1f5b9c
beisekin <mr.batkaval@gmail.com>
2021-12-16 12:51:35 +0000

    django0018 : models updates

--- backend/app/db_models/serializers/ser_company.py
+++ backend/app/db_models/serializers/ser_company.py
@@ -3,20 +3,8 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from ..models import Company
 
-
-# class CompanyListSerializer(serializers.ModelSerializer):
-#     company_ref = serializers.CharField(source='*')
-
-#     class Meta:
-#         model = Company
-#         fields = ('company_ref', )
-
 class CompanySerializer(serializers.ModelSerializer):
-    # company_ref = serializers.SlugRelatedField(many=False, read_only=False, slug_field="name")
-    # company_ref = serializers.PrimaryKeyRelatedField(
-    #     queryset=Company.objects.all(), source='company_ref', allow_null=False, required=True
-    # )
+    last_updt_user = serializers.HiddenField(default = serializers.CurrentUserDefault())
     class Meta:
         model = Company
         fields = '__all__'
-        # fields = ('id','name','parent', 'children')
--- backend/app/db_models/serializers/ser_field.py
+++ backend/app/db_models/serializers/ser_field.py
@@ -4,19 +4,8 @@ from rest_framework import serializers
 from ..models import Field
 
 
-# class CompanyListSerializer(serializers.ModelSerializer):
-#     company_ref = serializers.CharField(source='*')
-
-#     class Meta:
-#         model = Company
-#         fields = ('company_ref', )
-
 class FieldSerializer(serializers.ModelSerializer):
-    # company_ref = serializers.SlugRelatedField(many=False, read_only=False, slug_field="name")
-    # company_ref = serializers.PrimaryKeyRelatedField(
-    #     queryset=Company.objects.all(), source='company_ref', allow_null=False, required=True
-    # )
+    last_updt_user = serializers.HiddenField(default = serializers.CurrentUserDefault())
     class Meta:
         model = Field
-        fields = '__all__'
-        # fields = ('id','name','parent', 'children')
+        fields = '__all__'        
