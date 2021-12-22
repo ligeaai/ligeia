0bdb758b00125e8ff8d4a508bc0cd857c8dc04d1
beisekin <mr.batkaval@gmail.com>
2021-12-22 17:21:38 +0600

    django0031 : celery

--- backend/core/celery.py
+++ backend/core/celery.py
@@ -1,10 +1,11 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from backend.settings import base
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.development")
 
-app = Celery("backend")
-app.config_from_object("backend.settings.dev", namespace="CELERY"),
-app.autodiscover_tasks(lambda: base.INSTALLED_APPS)
+if os.environ.get('DJANGO_SETTINGS_MODULE', None) is None:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.dev')
+
+app = Celery("core")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
