447c9f25e238a15b3c61e9979796dc432b3b5d4d
beisekin <mr.batkaval@gmail.com>
2022-01-05 08:53:36 +0600

    web0003 : updates

--- todo/todo/src/components/todo-list/todo-list.js
+++ todo/todo/src/components/todo-list/todo-list.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import TodoListItem from './todo-list-item';
+import TodoListItem from '../todo-list-item';
 import './todo-list.css';
 
 const TodoList = ({ todos }) => {
 
-  const elements = todos.map((item, id) => {
-    const { ...itemProps } = item;
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
 
     return (
       <li key={id} className="list-group-item">
