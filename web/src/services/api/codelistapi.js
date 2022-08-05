fe2221ce4566d575b7b2574a0361b6101bc6749c
EmirAlpAksungur <77407633+EmirAlpAksungur@users.noreply.github.com>
2022-08-05 10:53:10 +0300

    webFolder0004 : spinner added

--- web/src/services/api/codelistapi.js
+++ web/src/services/api/codelistapi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const getAll = () => {
-    return  axios({
+    return axios({
         method: 'get',
         url: `http://127.0.0.1:8000/api/v1/code_list/`
     })
 };
 
-export default getAll;
+export {getAll};
