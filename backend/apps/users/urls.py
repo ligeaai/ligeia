4bae626f77b9f533909c78628ec54636a8b1cd90
beisekin <mr.batkaval@gmail.com>
2022-08-08 01:25:39 +0600

    docker-compose main updates

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path, re_path
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from knox import views as knox_views
 
 from .views import (
