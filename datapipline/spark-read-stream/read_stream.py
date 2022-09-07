906b67dac3375259bb32ac6af0f3b24e54c1b147
alpereneskici <59954776+alpereneskici@users.noreply.github.com>
2022-09-07 14:44:24 +0300

    spark-read-stream-update

--- datapipline/spark-read-stream/read_stream.py
+++ datapipline/spark-read-stream/read_stream.py
@@ -9,6 +9,7 @@ from pyspark.sql.functions import *
 
 appName = "PySpark Example - JSON file to Spark Data Frame"
 master = "local"
+time_format = "dd/MM/yyyy HH:mm:ss"
 
 spark = SparkSession \
     .builder \
@@ -19,12 +20,13 @@ spark = SparkSession \
 spark.sparkContext.setLogLevel("ERROR")
 
 schema = StructType([
-    StructField('date1', StringType(), True),
-    StructField('Temperature:', StringType(), True),
-    StructField('Pressure:', StringType(), True),
-    StructField('Vibration_X', StringType(), True),
-    StructField('Vibration_Y', StringType(), True),
-    StructField('Vibration_motor', StringType(), True)
+    StructField('Tarih', StringType(), True),
+    StructField('Time', TimestampType(), True),
+    StructField('Temperature:', FloatType(), True),
+    StructField('Pressure:', FloatType(), True),
+    StructField('Vibration_X', FloatType(), True),
+    StructField('Vibration_Y', FloatType(), True),
+    StructField('Vibration_motor', FloatType(), True)
 ])
 
 lines = spark \
@@ -32,22 +34,25 @@ lines = spark \
     .option('multiLine', True) \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "spark-kafka") \
+    .option("subscribe", "raw-data") \
     .option("startingOffsets", "earliest") \
     .load()\
     .select(from_json(col("value").cast("string"), schema).alias("parsed_value"))\
     .select(col("parsed_value.*"))
 df = lines.select('*')
 
+df2 = df\
+    .withColumn("org_unit4",split(col("Tarih"),",").getItem(0))\
+    .withColumn("asset",split(col("Tarih"),",").getItem(1))\
+    .drop("Tarih")\
+    .withColumn("Time",date_format(col("Time"),time_format))\
+    .select("org_unit4","asset","Time","Temperature:","Pressure:","Vibration_X","Vibration_Y","Vibration_motor")
 
-
-query = df \
+query = df2 \
     .writeStream \
+    .option("truncate", False)\
     .outputMode("Append") \
     .format("console") \
     .start()
-
-# raw = spark.sql("select * from `kafka-streaming-messages`")
-# raw.show()
 
 query.awaitTermination()
