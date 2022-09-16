61530c9beb633a5d4b610ded42355f715f623e73
alpereneskici <59954776+alpereneskici@users.noreply.github.com>
2022-09-17 00:26:35 +0300

    update-data

--- datapipline/spark-read-stream/read_stream.py
+++ datapipline/spark-read-stream/read_stream.py
@@ -1,7 +1,11 @@
 import findspark
+
 findspark.init()
 import os
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.1 pyspark-shell'
+
+os.environ[
+    "PYSPARK_SUBMIT_ARGS"
+] = "--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.1 pyspark-shell"
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
@@ -11,48 +15,60 @@ appName = "PySpark Example - JSON file to Spark Data Frame"
 master = "local"
 time_format = "dd/MM/yyyy HH:mm:ss"
 
-spark = SparkSession \
-    .builder \
-    .appName("Spark Kafka Streaming") \
-    .master(master) \
-    .getOrCreate()
+spark = (
+    SparkSession.builder.appName("Spark Kafka Streaming").master(master).getOrCreate()
+)
 
 spark.sparkContext.setLogLevel("ERROR")
 
-schema = StructType([
-    StructField('Tarih', StringType(), True),
-    StructField('Time', TimestampType(), True),
-    StructField('Temperature:', FloatType(), True),
-    StructField('Pressure:', FloatType(), True),
-    StructField('Vibration_X', FloatType(), True),
-    StructField('Vibration_Y', FloatType(), True),
-    StructField('Vibration_motor', FloatType(), True)
-])
+schema = StructType(
+    [
+        StructField("id", IntegerType(), True),
+        StructField("tarih", StringType(), True),
+        StructField("time", TimestampType(), True),
+        StructField("temperature", FloatType(), True),
+        StructField("pressure", FloatType(), True),
+        StructField("vibration_x", FloatType(), True),
+        StructField("vibration_y", FloatType(), True),
+        StructField("vibration_motor", FloatType(), True),
+    ]
+)
 
-lines = spark \
-    .readStream \
-    .option('multiLine', True) \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "raw-data") \
-    .option("startingOffsets", "earliest") \
-    .load()\
-    .select(from_json(col("value").cast("string"), schema).alias("parsed_value"))\
+lines = (
+    spark.readStream.option("multiLine", True)
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("subscribe", "raw-data")
+    .option("startingOffsets", "earliest")
+    .load()
+    .select(from_json(col("value").cast("string"), schema).alias("parsed_value"))
     .select(col("parsed_value.*"))
-df = lines.select('*')
+)
+df = lines.select("*")
 
-df2 = df\
-    .withColumn("org_unit4",split(col("Tarih"),",").getItem(0))\
-    .withColumn("asset",split(col("Tarih"),",").getItem(1))\
-    .drop("Tarih")\
-    .withColumn("Time",date_format(col("Time"),time_format))\
-    .select("org_unit4","asset","Time","Temperature:","Pressure:","Vibration_X","Vibration_Y","Vibration_motor")
+df2 = (
+    df.withColumn("org_unit4", split(col("tarih"), ",").getItem(0))
+    .withColumn("asset", split(col("tarih"), ",").getItem(1))
+    .drop("tarih")
+    .withColumn("time", date_format(col("time"), time_format))
+    .select(
+        "id",
+        "org_unit4",
+        "asset",
+        "time",
+        "temperature",
+        "pressure",
+        "vibration_x",
+        "vibration_y",
+        "vibration_motor",
+    )
+)
 
-query = df2 \
-    .writeStream \
-    .option("truncate", False)\
-    .outputMode("Append") \
-    .format("console") \
+query = (
+    df2.writeStream.option("truncate", False)
+    .outputMode("Append")
+    .format("console")
     .start()
+)
 
 query.awaitTermination()
