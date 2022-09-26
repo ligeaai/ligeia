63de2b46832ea7634977a9b89cdb7b16b11fbd8e
beisekin <mr.batkaval@gmail.com>
2022-09-26 12:15:22 +0600

    (fe0) 4. React - change to camel casing naming convesions , whole web folder

--- web/src/pages/App.js
+++ web/src/pages/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 import { useSelector } from "react-redux";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-import AppRouter from '../routers/appRouter'
+import AppRouter from "../routers/appRouter";
 
-import Loading from '../components/loading';
+import Loading from "../components/loading";
 
 const App = () => {
-    const theme = useSelector((state) => state.theme.theme);
-    const darkTheme = createTheme({
-        palette: {
-            mode: theme,
-        },
-    });
-    return (
-        <ThemeProvider theme={darkTheme}>
-            <Loading Element={<AppRouter />} />
-        </ThemeProvider>
-    );
-}
+  const theme = useSelector((state) => state.theme.theme);
+  const darkTheme = createTheme({
+    palette: {
+      mode: theme,
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Loading Element={<AppRouter />} />
+    </ThemeProvider>
+  );
+};
 
 export default App;
