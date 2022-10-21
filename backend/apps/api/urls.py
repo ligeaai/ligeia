7a4035ce92264abacae370c67e55bc68f0f02685
beisekin <mr.batkaval@gmail.com>
2022-10-21 20:37:22 +0600

    code_list check

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -2,12 +2,12 @@ from django.urls import include, path
 
 urlpatterns = [
     path("auth/", include("apps.users.urls")),
+    path("layer/", include("apps.layer.urls")),
     path("type/", include("apps.type.urls")),
     path("type-link/", include("apps.type_link.urls")),
     path("type-property/", include("apps.type_property.urls")),
+    path("item/", include("apps.item.urls")),
+    path("item-property/", include("apps.item_property.urls")),
     path("code-list/", include("apps.code_list.urls")),
     path("resource-list/", include("apps.resource_list.urls")),
-    path("item/", include("apps.item.urls")),
-    path("layer/", include("apps.layer.urls")),
-    path("item-property/", include("apps.item_property.urls")),
 ]
