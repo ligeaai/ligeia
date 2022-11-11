06743b4823e4c0e1ebbb6ec1609064bc23fcf41c
Umut Bilgen <84351356+UmutBilgen@users.noreply.github.com>
2022-11-11 16:22:49 +0300

    item link add to admin panel

--- backend/apps/item_link/admin.py
+++ backend/apps/item_link/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import item_link
+# Register your models here.
+admin.site.register(item_link)
 # Register your models here.
--- backend/apps/item_link/views.py
+++ backend/apps/item_link/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics,permissions,status
 from rest_framework.response import Response 
 from .models import item_link
-from .serializers import ItemLinkSaveSerializer
+from .serializers import ItemLinkSaveSerializer,ItemLinkDetailsSerializer
 from utils.models_utils import validate_find
 # Create your views here.
 
@@ -22,8 +22,9 @@ class ItemLinkDetailsView(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         quaryset  = item_link.objects.filter(TO_ITEM_ID = request.data.get("TO_ITEM_ID"))
+        print()
         validate_find(quaryset,request)
-        serializer = ItemLinkSaveSerializer(quaryset,many = True)
+        serializer = ItemLinkDetailsSerializer(quaryset,many = True)
         return Response(serializer.data,status=status.HTTP_200_OK)
         
 
