3689817f3e655f80707dd4c48e5a86c1673d1278
UmutBilgen <umut.reis.166@gmail.com>
2022-11-24 21:46:27 +0000

    Fix

--- backend/apps/tags/views.py
+++ backend/apps/tags/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import tags
+import uuid
 from rest_framework import permissions,status,generics
 from rest_framework.response import Response
 from .serializers import TagsDetiailsSerializer,TagsSaveSerializer,TagsFieldsSerializer
@@ -10,6 +11,8 @@ from apps.type_property.serializers import TypePropertyDetailsSerializer
 from apps.item_link.models import item_link 
 from apps.item_link.serializers import ItemLinkSaveSerializer
 from apps.type_link.models import type_link 
+from apps.code_list.models import code_list
+from apps.templates.orm_CodeList import CodeListORM
 from apps.type_link.serializers import TypeLinkDetailsSerializer
 from utils.models_utils import validate_model_not_null
 # Create your views here.
@@ -19,14 +22,24 @@ class TagsSaveView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
-        validate_model_not_null(request.data.get('TAGS'),'tags',request = request)
-        validate_model_not_null(request.data.get('LINK'),"ITEM_LINK",request = request)
+        tags_dict = request.data
+        link_dict = dict()
+        try:
+            links_list = ['LINK_ID', 'LINK_TYPE', 'START_DATETIME', 'END_DATETIME', 'FROM_ITEM_ID', 'FROM_ITEM_TYPE', 'TO_ITEM_ID', 'TO_ITEM_TYPE']
+            for keys in links_list:
+                link_dict[keys] = request.data.get(keys)
+                tags_dict.pop(keys)
+        except:
+            pass 
+        link_dict['ROW_ID'] = uuid.uuid4().hex
+        validate_model_not_null(tags_dict,'tags',request = request)
+        validate_model_not_null(link_dict,"ITEM_LINK",request = request)
         serializer = TagsSaveSerializer(data = request)
         serializer.is_valid()
-        message = serializer.save(request.data.get('TAGS'))
+        message = serializer.save(tags_dict)
         link_serializer = ItemLinkSaveSerializer(data = request.data.get('LINK'))
         link_serializer.is_valid()
-        message = link_serializer.save(request.data.get('LINK'))
+        message = link_serializer.save(link_dict)
         
         return Response(message,status=status.HTTP_200_OK)
     
@@ -74,11 +87,17 @@ class TagsPropertysView(generics.CreateAPIView):
 
     def _resourceLabel(self,data,dataList,culture):
         for item in (data):
+            if item.get('CODE_LIST'):
+                qs_codeList = code_list.objects.filter(LIST_TYPE = "CODE_LIST",CODE = item.get('CODE_LIST'))
+                if not qs_codeList:
+                    qs_codeList = code_list.objects.filter(LIST_TYPE = item.get('CODE_LIST'))
+                item['CODE'] = CodeListORM.getCodeList(qs_codeList,culture=culture,hierarchy=False)
+
             qs_resource = resource_list.objects.filter(ID = item.get('LABEL_ID'),CULTURE = culture)
             if qs_resource:
                 serializer_rs = ResourceListSerializer(qs_resource,many = True)
                 item['SHORT_LABEL'] = serializer_rs.data[0].get('SHORT_LABEL')
-                item['SHORT_LABEL'] = serializer_rs.data[0].get('MOBILE_LABEL')
+                item['MOBILE_LABEL'] = serializer_rs.data[0].get('MOBILE_LABEL')
                 dataList.append(item)
 
 
