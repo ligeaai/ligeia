11532e8d0f7e1a4f2888eb00a501f1eb091ac666
UmutBilgen <umut.reis.166@gmail.com>
2022-12-30 22:09:54 +0000

    backfill data for django channels basic

--- backend/apps/websocket/routing.py
+++ backend/apps/websocket/routing.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .consumers import WSConsumer
+from .consumers import WSConsumer,WSConsumerBackfill
 
 websocket_urlpatterns = [
 	path('ws/tags/', WSConsumer.as_asgi()),
+	path('ws/tags/backfill/', WSConsumerBackfill.as_asgi()),
 ]
