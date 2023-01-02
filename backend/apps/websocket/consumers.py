cc20612cd4a52330982933a70453ec70655f6264
UmutBilgen <umut.reis.166@gmail.com>
2023-01-02 16:13:43 +0000

    backfill websocket connection lost error fixed

--- backend/apps/websocket/consumers.py
+++ backend/apps/websocket/consumers.py
@@ -21,34 +21,22 @@ class WSConsumer(WebsocketConsumer):
 
 
 
+
 class WSConsumerBackfill(WebsocketConsumer):
 	def connect(self):
 		self.accept()
-		import environ
-		import prometheus_client as prom
-		import random
-		import json
-		import time
-		from prometheus_client import start_http_server
-		# Create a metric to track time spent and requests made.
-		from cassandra.cluster import Cluster
+
+
+		from cassandra.cluster import Cluster	
 		from cassandra.auth import PlainTextAuthProvider
 		from cassandra.query import ordered_dict_factory
 
-		env = environ.Env(DEBUG=(bool, False))
-		cassandra_host = "34.70.46.54"
-		cassandra_port = 9092
-		print('GİRDİ')
-		counter = prom.Counter('python_my_counter', 'This is my counter')
-		gauge = prom.Gauge('python_my_gauge', 'This is my gauge')
-		histogram = prom.Histogram('python_my_histogram', 'This is my histogram')
-		summary = prom.Summary('python_my_summary', 'This is my summary')
 
 		auth_provider = PlainTextAuthProvider(username ='cassandra', password='cassandra')
 		cluster=Cluster(['cassandra'], auth_provider=auth_provider)
-
 		session = cluster.connect('backfilldata')
 		session.row_factory = ordered_dict_factory
+	
 		rows = session.execute('SELECT * FROM backfilldata')
 		for row in rows:
 			self.send(json.dumps({'message':row}))
