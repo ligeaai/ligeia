43ddb1ac42c09651a553bd40c05dba0db1554157
UmutBilgen <umut.reis.166@gmail.com>
2023-01-06 17:38:27 +0000

    alarms web Ui web socker

--- backend/apps/websocket/consumers.py
+++ backend/apps/websocket/consumers.py
@@ -1,30 +1,47 @@
 import json
-from time import sleep
+import environ
 from channels.generic.websocket import WebsocketConsumer
 import redis
-import environ
-from channels.exceptions import StopConsumer
+from cassandra.cluster import Cluster	
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.query import ordered_dict_factory
+
+
 
 env = environ.Env(DEBUG=(bool, False))
 
+class WSLiveConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        rds = redis.StrictRedis(env('REDIS_HOST'),port=6379,db=1)
+        count = 0
+        for i in rds.keys():
+            data = rds.get(i)
+            data = data.decode('utf-8')
+            data = json.loads(data)
+            self.send(json.dumps({'message':data}))
+    
+    def disconnect(self, close_code):
+        print('disconnect')
 
-class WSConsumer(WebsocketConsumer):
-	def connect(self):
-		self.accept()
-		rds = redis.StrictRedis(env('REDIS_HOST'),port=6379,db=1)
-		count = 0
-		for i in rds.keys():
-			data = rds.get(i)
-			data = data.decode('utf-8')
-			data = json.loads(data)
-			self.send(json.dumps({'message':data}))
-	
-	def disconnect():
-           raise StopConsumer
+class AlarmsConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        print('CONNECT')
 
+    def disconnect(self, close_code):
+        print('disconnect')
 
-
-
+    def receive(self, text_data):
+        rds = redis.StrictRedis(env('REDIS_HOST'),port=6379,db=2)
+        for i in rds.keys():
+            data = rds.get(i)
+            data = data.decode('utf-8')
+            data = json.loads(data)
+            if data.get('LOG_TYPE') == 'ALARMS':
+                if data.get('CONTENTS').get('quality') > int(text_data): # item LAYER match tag_naame
+                    self.send(json.dumps({'message':data.get('CONTENTS')}))
+                    
 
 
 class WSConsumerBackfill(WebsocketConsumer):
@@ -44,5 +61,5 @@ class WSConsumerBackfill(WebsocketConsumer):
 		for row in rows:
 			self.send(json.dumps({'message':row}))
 
-	def disconnect():
-           raise StopConsumer
+	def disconnect(self,close_code):
+           print('DİSCONNECT')
