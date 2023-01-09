eda7d79686a8555a3fa091923543197fc54646d2
EmirAlpAksungur <77407633+EmirAlpAksungur@users.noreply.github.com>
2023-01-10 00:58:54 +0300

    React - date formats for timeStamp on widgets should be dd/mm/yyyy HH:MM

--- frontend/src/services/utils/dateFormatter.js
+++ frontend/src/services/utils/dateFormatter.js
@@ -13,9 +13,10 @@ function addZero(i) {
 }
 
 export function dateFormatDDMMYYHHMM(date) {
-    var d = date.getDate();
+    var d = addZero(date.getDate());
     var m = date.getMonth();
     m += 1;
+    m = addZero(m)
     var y = date.getFullYear();
     var h = addZero(date.getHours());
     var min = addZero(date.getMinutes());
