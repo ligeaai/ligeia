b82cb07312dedb31dd31fc680ddde2280e5722c9
UmutBilgen <umut.reis.166@gmail.com>
2023-01-09 23:33:49 +0000

    tags hierarchy and tag name

--- backend/apps/item_link/urls.py
+++ backend/apps/item_link/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path, re_path
 from django.urls.resolvers import URLPattern
 
-from .views import ItemLinkSaveView,ItemLinkDeleteView,ItemLinkDetailsView,ItemLinkUpdateView,ItemLinkHierarchyView,ItemLinkCardinaltyView
+from .views import ItemLinkSaveView,ItemLinkDeleteView,ItemLinkDetailsView,ItemLinkUpdateView,ItemLinkHierarchyView,ItemLinkCardinaltyView,TagsLinksView
 
 urlpatterns = [
     path("save/", ItemLinkSaveView.as_view(), name="itemlinksave"),
@@ -10,5 +10,6 @@ urlpatterns = [
     path("details/", ItemLinkDetailsView.as_view(), name="itemlinkdetails"),
     path("hierarchy/", ItemLinkHierarchyView.as_view(), name="hierarchy"),
     path("cardinalty/", ItemLinkCardinaltyView.as_view(), name="cardinalty"),
+    path("tags/", TagsLinksView.as_view(), name="cardinalty"),
    
 ]
