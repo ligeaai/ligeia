8d281dbce07bd910dd3cd39c4d94cac72d192607
UmutBilgen <umut.reis.166@gmail.com>
2023-01-10 00:34:06 +0000

    all uom base unit end points and uom model delete

--- backend/apps/uom/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import uom
-
-class UomDetailsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = uom
-        fields = '__all__'
--- backend/apps/uom_base_unit/urls.py
+++ backend/apps/uom_base_unit/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path, re_path
 from django.urls.resolvers import URLPattern
 
-from .views import (UomUnitSaveView,UomUnitScriptView,UomUnitDetialsView)
+from .views import (UomUnitSaveView,UomUnitScriptView,UomUnitDetialsView,UomUnitsDetailView)
 
 urlpatterns = [
     
     path("save/", UomUnitSaveView.as_view(),name='UomSave'),
-    path("details/", UomUnitDetialsView.as_view(),name='details'),
+    path("detail/", UomUnitDetialsView.as_view(),name='detail'),
+    path("details/", UomUnitsDetailView.as_view(),name='details'),
     path("scripts/", UomUnitScriptView.as_view(),name='Scripts'),
 
     
