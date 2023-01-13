86ced578f5ff7dabd30b070edd017850e0982f63
UmutBilgen <umut.reis.166@gmail.com>
2023-01-13 09:29:03 +0000

    (be0) Django -end point for UOM type, name,symbol

--- backend/apps/uom_base_unit/views.py
+++ backend/apps/uom_base_unit/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics, permissions,status
 from rest_framework.response import Response
 from .models import uom_base_unit
-from .serializers import UomUnitDetailsSerializer
+from .serializers import UomUnitDetailsSerializer,UomUnitQuantitySerializer
 import uuid
-
+from apps.uoms.models import uom
+from apps.uoms.serializers import UomQuantitySerializer
 from services.parsers.addData.type import typeAddData
 
 class UomUnitSaveView(generics.CreateAPIView):
@@ -35,9 +36,9 @@ class UomUnitDetialsView(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         queryset = uom_base_unit.objects.filter(RP66_SYMBOL = request.data.get('UOM'))
-      
         serializer = UomUnitDetailsSerializer(queryset,many = True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class UomUnitsDetailView(generics.ListAPIView):
     serializer_class = UomUnitDetailsSerializer
@@ -46,3 +47,17 @@ class UomUnitsDetailView(generics.ListAPIView):
         queryset = uom_base_unit.objects.all()
         serializer = UomUnitDetailsSerializer(queryset,many = True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UomQuantityTypeDetailView(generics.ListAPIView):
+    serializer_class = UomUnitQuantitySerializer
+    permission_classes = [permissions.AllowAny]
+    def get_queryset(self):
+        pass
+    def get(self, request, *args, **kwargs):
+        queryset = uom_base_unit.objects.all().distinct("QUANTITY_TYPE")
+        queryset2 = uom.objects.all().distinct("QUANTITY_TYPE")
+        serializerUnit = UomUnitQuantitySerializer(queryset,many = True)
+        serializer = UomQuantitySerializer(queryset2,many = True)
+        data = list(serializerUnit.data) + list(serializer.data)
+        return Response(data, status=status.HTTP_200_OK)
