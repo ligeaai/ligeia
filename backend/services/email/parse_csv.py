b2dad23fc90cd5203c491e68a1010c2b1ecdc975
alpereneskici <alperen148577@gmail.com>
2023-01-26 21:08:19 +0300

    edit:parse_csv

--- backend/services/email/parse_csv.py
+++ backend/services/email/parse_csv.py
@@ -6,37 +6,40 @@ from dateutil import parser
 import os
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers=os.environ.get('Kafka_Host'),
-                                      value_serializer=lambda v: json.dumps(v).encode('utf-8'),
-                                    linger_ms=10)
+producer = KafkaProducer(
+    bootstrap_servers=os.environ.get("Kafka_Host"),
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    linger_ms=10,
+)
 file_name = ""
 with os.scandir(os.getcwd()) as find:
     for file in find:
         if file.name.endswith("csv"):
-            file_name = (file.name)
-data = pd.read_csv(file_name,sep = ';',encoding='latin-1')
+            file_name = file.name
+data = pd.read_csv(file_name, sep=";", encoding="WINDOWS-1251")
 
 keys = data.keys()
 temp = []
 for row_index in range(len(data)):
-    for keys_index in range(6,len(keys)):
+    for keys_index in range(6, len(keys)):
         raw_data = {}
         raw_data["completion"] = data[keys[1]][row_index]
         raw_data["created_by"] = data[keys[4]][row_index]
         raw_data["date"] = data[keys[5]][row_index]
-        raw_data['tag_name'] = keys[keys_index].split(',')[0]
+        raw_data["tag_name"] = keys[keys_index].split(",")[0]
         try:
-            datetime_object = parser.parse(raw_data.get('date'))
+            datetime_object = parser.parse(raw_data.get("date"))
         except:
             continue
-        if len(keys[keys_index].split(',')) > 1:
-            raw_data['uom'] = keys[keys_index].split(',')[1]
+        if len(keys[keys_index].split(",")) > 1:
+            raw_data["uom"] = keys[keys_index].split(",")[1]
         else:
-            raw_data['uom'] = "blank"
-        
+            raw_data["uom"] = "blank"
+
         if data[keys[keys_index]][row_index] is not None:
             raw_data["tag_value"] = float(data[keys[keys_index]][row_index])
-            producer.send(os.environ.get('Kafka_Topic_Raw_Data'), value = raw_data)
+            raw_data = json.dumps(raw_data, ensure_ascii=False)
+            producer.send(os.environ.get("Kafka_Topic_Raw_Data"), value=raw_data)
         else:
             continue
 
