850f4cd88d286ddb3693076b0640b9a4178ced25
beisekin <mr.batkaval@gmail.com>
2023-02-06 13:17:27 +0600

    Django - PgHS refactor

--- backend/services/healt_status/postgresql_health_status.py
+++ backend/services/healt_status/postgresql_health_status.py
@@ -1,16 +1,14 @@
 import psycopg2
 import os
-import datetime
-from kafka import KafkaProducer
 import json
+from kafka import KafkaProducer
+from datetime import datetime
 
 host = os.environ.get("Kafka_Host_DP")
 producer = KafkaProducer(
     bootstrap_servers=host,
     value_serializer=lambda v: json.dumps(v).encode("ascii"),
 )
-created_time = datetime.datetime.now()
-
 
 def HealthCheckForPostgre():
     try:
@@ -24,12 +22,11 @@ def HealthCheckForPostgre():
             print("PostgreSQL is up and running!")
         else:
             print("PostgreSQL is not running.")
-
-    except Exception as e:
+    except (psycopg2.OperationalError, psycopg2.DatabaseError) as e:
         error_message = ("Error connecting to PostgreSQL:", str(e))
         data = {
             "LOG_TYPE": "Alarm",
-            "date": created_time,
+            "date": str(datetime.now()),
             "layer_name": "KNOC",
             "error_message": error_message,
             "container": "Postgre-SQL",
@@ -37,5 +34,49 @@ def HealthCheckForPostgre():
         producer.send("alarms", value=data)
         producer.flush()
     finally:
+        cur.close()
         conn.close()
-        print("finally")
+    print("finally")
+
+
+# import psycopg2
+# import os
+# import datetime
+# from kafka import KafkaProducer
+# import json
+
+# host = os.environ.get("Kafka_Host_DP")
+# producer = KafkaProducer(
+#     bootstrap_servers=host,
+#     value_serializer=lambda v: json.dumps(v).encode("ascii"),
+# )
+# created_time = datetime.datetime.now()
+
+
+# def HealthCheckForPostgre():
+#     try:
+#         conn = psycopg2.connect(
+#             host="postgres", database="postgres", user="postgres", password="manager"
+#         )
+#         cur = conn.cursor()
+#         cur.execute("SELECT 1")
+#         result = cur.fetchone()
+#         if result == (1,):
+#             print("PostgreSQL is up and running!")
+#         else:
+#             print("PostgreSQL is not running.")
+
+#     except Exception as e:
+#         error_message = ("Error connecting to PostgreSQL:", str(e))
+#         data = {
+#             "LOG_TYPE": "Alarm",
+#             "date": created_time,
+#             "layer_name": "KNOC",
+#             "error_message": error_message,
+#             "container": "Postgre-SQL",
+#         }
+#         producer.send("alarms", value=data)
+#         producer.flush()
+#     finally:
+#         conn.close()
+#         print("finally")
