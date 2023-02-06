620b5a0e87c44561b4c3c594e5bc6248d103ad79
beisekin <mr.batkaval@gmail.com>
2023-02-06 13:34:47 +0600

    Django - DHS

--- backend/services/healt_status/django_health_status.py
+++ backend/services/healt_status/django_health_status.py
@@ -5,14 +5,24 @@ from kafka import KafkaProducer
 import json
 
 host = os.environ["Kafka_Host_DP"]
-producer = KafkaProducer(
-    bootstrap_servers=host,
-    value_serializer=lambda v: json.dumps(v).encode("ascii"),
-)
-
 base_url = os.environ["BASE_URL"]
 url = f"{base_url}:8000/api/v1/health"
 created_time = datetime.datetime.now()
+
+def send_alarm(error_message):
+    data = {
+        "LOG_TYPE": "Alarm",
+        "date": created_time,
+        "layer_name": "KNOC",
+        "error_message": error_message,
+        "container": "Django",
+    }
+    with KafkaProducer(
+        bootstrap_servers=host,
+        value_serializer=lambda v: json.dumps(v).encode("ascii"),
+    ) as producer:
+        producer.send("alarms", value=data)
+
 try:
     response = requests.get(url)
     if response.status_code == 200:
@@ -21,13 +31,39 @@ try:
         raise Exception(f"Django health check failed: HTTP {response.status_code}")
 
 except Exception as e:
-    error_message = f"Error: Could not connect to Django: {str(e)}"
-    data = {
-        "LOG_TYPE": "Alarm",
-        "date": created_time,
-        "layer_name": "KNOC",
-        "error_message": error_message,
-        "container": "Django",
-    }
-    producer.send("alarms", value=data)
-    producer.flush()
+    send_alarm(f"Error: Could not connect to Django: {str(e)}")
+
+
+# import requests
+# import os
+# import datetime
+# from kafka import KafkaProducer
+# import json
+
+# host = os.environ.get("Kafka_Host_DP")
+# producer = KafkaProducer(
+#     bootstrap_servers=host,
+#     value_serializer=lambda v: json.dumps(v).encode("ascii"),
+# )
+
+# base_url = os.environ.get("BASE_URL")
+# url = base_url + ":8000/api/v1/health"
+# created_time = datetime.datetime.now()
+# try:
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         print("Django health check: OK")
+#     else:
+#         print("Django health check failed: HTTP {}".format(response.status_code))
+
+# except Exception as e:
+#     error_message = "Error: Could not connect to Django:", str(e)
+#     data = {
+#         "LOG_TYPE": "Alarm",
+#         "date": created_time,
+#         "layer_name": "KNOC",
+#         "error_message": error_message,
+#         "container": "Django",
+#     }
+#     producer.send("alarms", value=data)
+#     producer.flush()
