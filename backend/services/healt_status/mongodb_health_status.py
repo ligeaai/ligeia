618736358f327919685a53eb30edd66ddb116b4c
beisekin <mr.batkaval@gmail.com>
2023-02-06 12:33:33 +0600

    Django - MHS refactor

--- backend/services/healt_status/mongodb_health_status.py
+++ backend/services/healt_status/mongodb_health_status.py
@@ -5,14 +5,10 @@ from kafka import KafkaProducer
 import json
 
 host = os.environ.get("Kafka_Host_DP")
-producer = KafkaProducer(
-    bootstrap_servers=host,
-    value_serializer=lambda v: json.dumps(v).encode("ascii"),
-)
 created_time = datetime.datetime.now()
+
 try:
     client = MongoClient("mongodb://root:admin@mongodb-timescale:27017/")
-    # Connect to the database and count the number of documents in a collection
     db = client["test_database"]
     coll = db["test_collection"]
     count = coll.count_documents({})
@@ -20,7 +16,7 @@ try:
     print("MongoDB health check: OK")
 
 except Exception as e:
-    error_message = ("Error: Could not connect to MongoDB:", str(e))
+    error_message = f"Error: Could not connect to MongoDB: {e}"
     data = {
         "LOG_TYPE": "Alarm",
         "date": created_time,
@@ -28,5 +24,11 @@ except Exception as e:
         "error_message": error_message,
         "container": "Mongo-db",
     }
+
+    producer = KafkaProducer(
+        bootstrap_servers=host,
+        value_serializer=lambda v: json.dumps(v).encode("ascii"),
+    )
     producer.send("alarms", value=data)
     producer.flush()
+    producer.close()
