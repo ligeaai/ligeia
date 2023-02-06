041b93f624f410d102727052482ff46927e20fa2
beisekin <mr.batkaval@gmail.com>
2023-02-06 12:22:27 +0600

    Django - DHS refactor

--- backend/services/healt_status/django_health_status.py
+++ backend/services/healt_status/django_health_status.py
@@ -4,24 +4,24 @@ import datetime
 from kafka import KafkaProducer
 import json
 
-host = os.environ.get("Kafka_Host_DP")
+host = os.environ["Kafka_Host_DP"]
 producer = KafkaProducer(
     bootstrap_servers=host,
     value_serializer=lambda v: json.dumps(v).encode("ascii"),
 )
 
-base_url = os.environ.get("BASE_URL")
-url = base_url + ":8000/api/v1/health"
+base_url = os.environ["BASE_URL"]
+url = f"{base_url}:8000/api/v1/health"
 created_time = datetime.datetime.now()
 try:
     response = requests.get(url)
     if response.status_code == 200:
         print("Django health check: OK")
     else:
-        print("Django health check failed: HTTP {}".format(response.status_code))
+        raise Exception(f"Django health check failed: HTTP {response.status_code}")
 
 except Exception as e:
-    error_message = "Error: Could not connect to Django:", str(e)
+    error_message = f"Error: Could not connect to Django: {str(e)}"
     data = {
         "LOG_TYPE": "Alarm",
         "date": created_time,
