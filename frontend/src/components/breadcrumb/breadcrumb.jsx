4bc310534d56f1362d9da89cf484a242ac31ac90
beisekin <mr.batkaval@gmail.com>
2023-02-06 18:32:24 +0600

    React - breadcrumb refactor

--- frontend/src/components/breadcrumb/breadcrumb.jsx
+++ frontend/src/components/breadcrumb/breadcrumb.jsx
@@ -1,47 +1,41 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { Breadcrumbs as MUIBreadcrumbs, Typography } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import history from "../../routers/history";
-
 import { confirmationPushHistory } from "../../services/utils/historyPush";
-import { setGoFunctionConfirmation } from "../../services/actions/confirmation/historyConfirmation";
+import { setGoFunctionConfirmation, selectGoFunction } from "../../services/actions/confirmation/historyConfirmation";
+
 const Breadcrumbs = () => {
   const dispatch = useDispatch();
-  var pathnames = decodeURI(window.location.pathname)
+  const goFunction = useSelector(selectGoFunction);
+  
+  const pathnames = decodeURI(window.location.pathname)
     .split("/")
-    .filter((x) => x);
-  const { params } = useParams(); //  important for updating breadcrumb
+    .filter(x => x);
+
+  const handleClick = (routeTo) => {
+    dispatch(setGoFunctionConfirmation(() => history.push(routeTo)));
+    dispatch(confirmationPushHistory());
+  };
+
   return (
-    <MUIBreadcrumbs
-      aria-label="breadcrumb"
-      separator="\"
-      sx={{
-        ".MuiBreadcrumbs-separator": {
-          mx: 0.5,
-        },
-      }}
-    >
+    <MUIBreadcrumbs aria-label="breadcrumb" separator="\" sx={{ ".MuiBreadcrumbs-separator": { mx: 0.5 } }}>
       <HomeIcon
         fontSize="small"
         sx={{
           position: "relative",
           top: "3px",
           cursor: "pointer",
-          color: "icon.primary",
+          color: "icon.primary"
         }}
-        onClick={() => {
-          dispatch(setGoFunctionConfirmation(() => history.push("home")));
-          dispatch(confirmationPushHistory());
-        }}
+        onClick={() => handleClick("home")}
       />
 
       {pathnames.map((name, index) => {
         const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
         const isLast = index === pathnames.length - 1;
-        name = name.replace(/_/g, " ");
-        name = name.replace(/%20/g, " ");
+        name = name.replace(/_/g, " ").replace(/%20/g, " ");
         return isLast ? (
           <Typography
             key={name}
@@ -50,7 +44,7 @@ const Breadcrumbs = () => {
               color: "primary.main",
               cursor: "pointer",
               textTransform: "capitalize",
-              fontWeight: "bold",
+              fontWeight: "bold"
             }}
           >
             {name}
@@ -58,15 +52,12 @@ const Breadcrumbs = () => {
         ) : (
           <Typography
             key={name}
-            onClick={() => {
-              dispatch(setGoFunctionConfirmation(() => history.push(routeTo)));
-              dispatch(confirmationPushHistory());
-            }}
+            onClick={() => handleClick(routeTo)}
             sx={{
               fontSize: "14px",
               color: "text.primary",
               cursor: "pointer",
-              textTransform: "capitalize",
+              textTransform: "capitalize"
             }}
           >
             {name}
