b95893ca7bf97ff44869f8e2f2f712a606d7789d
beisekin <mr.batkaval@gmail.com>
2023-02-06 20:25:06 +0600

    React - button refactor

--- frontend/src/components/buttons/socialButton.jsx
+++ frontend/src/components/buttons/socialButton.jsx
@@ -1,26 +1,57 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 
-import { Box } from "@mui/material";
+const useStyles = makeStyles({
+  root: {
+    width: "5rem",
+    height: "3.5rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "8px",
+    cursor: "pointer",
+    color: "text.primary",
+    border: 1,
+  },
+});
 
-const socialButton = (props) => {
+const SocialButton = (props) => {
   const { Logo } = props;
+  const classes = useStyles();
+
   return (
-    <Box
-      sx={{
-        width: "5rem",
-        height: "3.5rem",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: "8px",
-        cursor: "pointer",
-        color: "text.primary",
-        border: 1,
-      }}
-    >
-      <Logo sx={{ typography: "h4" }} />
+    <Box className={classes.root}>
+      <Logo fontSize="h4" />
     </Box>
   );
 };
 
-export default socialButton;
+export default SocialButton;
+
+// import React from "react";
+
+// import { Box } from "@mui/material";
+
+// const socialButton = (props) => {
+//   const { Logo } = props;
+//   return (
+//     <Box
+//       sx={{
+//         width: "5rem",
+//         height: "3.5rem",
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         borderRadius: "8px",
+//         cursor: "pointer",
+//         color: "text.primary",
+//         border: 1,
+//       }}
+//     >
+//       <Logo sx={{ typography: "h4" }} />
+//     </Box>
+//   );
+// };
+
+// export default socialButton;
