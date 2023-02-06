b7bacecbc48f3a7d9ebb52a0f7ffc0663415fe32
beisekin <mr.batkaval@gmail.com>
2023-02-06 17:35:02 +0600

    React - error comp refactor

--- frontend/src/components/errorMessage/componentErrorBody.jsx
+++ frontend/src/components/errorMessage/componentErrorBody.jsx
@@ -1,19 +1,36 @@
 import React from "react";
-
 import { Grid } from "@mui/material";
 
-const componentErrorBody = (props) => {
-  const { icon, text } = props;
-  return (
-    <Grid container>
-      <Grid item xs={12} sx={{ textAlign: "center", color: "primary.main" }}>
-        {icon}
-      </Grid>
-      <Grid item xs={12} sx={{ textAlign: "center", color: "primary.main" }}>
-        {text}
-      </Grid>
+const ComponentErrorBody = ({ icon, text }) => (
+  <Grid container alignItems="center" justify="center">
+    <Grid item xs={12} sx={{ textAlign: "center", color: "primary.main" }}>
+      {icon}
     </Grid>
-  );
-};
+    <Grid item xs={12} sx={{ textAlign: "center", color: "primary.main" }}>
+      {text}
+    </Grid>
+  </Grid>
+);
 
-export default componentErrorBody;
+export default ComponentErrorBody;
+
+
+// import React from "react";
+
+// import { Grid } from "@mui/material";
+
+// const componentErrorBody = (props) => {
+//   const { icon, text } = props;
+//   return (
+//     <Grid container>
+//       <Grid item xs={12} sx={{ textAlign: "center", color: "primary.main" }}>
+//         {icon}
+//       </Grid>
+//       <Grid item xs={12} sx={{ textAlign: "center", color: "primary.main" }}>
+//         {text}
+//       </Grid>
+//     </Grid>
+//   );
+// };
+
+// export default componentErrorBody;
