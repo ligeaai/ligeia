6ec478841f784d2d794fc3ae9e13281ca1317a57
beisekin <mr.batkaval@gmail.com>
2023-02-06 13:30:57 +0600

    Django - MHS

--- backend/services/healt_status/mongodb_health_status.py
+++ backend/services/healt_status/mongodb_health_status.py
@@ -6,13 +6,12 @@ import json
 
 host = os.environ.get("Kafka_Host_DP")
 created_time = datetime.datetime.now()
+client = MongoClient("mongodb://root:admin@mongodb-timescale:27017/")
+db = client["test_database"]
+coll = db["test_collection"]
 
 try:
-    client = MongoClient("mongodb://root:admin@mongodb-timescale:27017/")
-    db = client["test_database"]
-    coll = db["test_collection"]
     count = coll.count_documents({})
-
     print("MongoDB health check: OK")
 
 except Exception as e:
@@ -24,7 +23,6 @@ except Exception as e:
         "error_message": error_message,
         "container": "Mongo-db",
     }
-
     producer = KafkaProducer(
         bootstrap_servers=host,
         value_serializer=lambda v: json.dumps(v).encode("ascii"),
@@ -32,3 +30,36 @@ except Exception as e:
     producer.send("alarms", value=data)
     producer.flush()
     producer.close()
+
+# from pymongo import MongoClient
+# import os
+# import datetime
+# from kafka import KafkaProducer
+# import json
+
+# host = os.environ.get("Kafka_Host_DP")
+# producer = KafkaProducer(
+#     bootstrap_servers=host,
+#     value_serializer=lambda v: json.dumps(v).encode("ascii"),
+# )
+# created_time = datetime.datetime.now()
+# try:
+#     client = MongoClient("mongodb://root:admin@mongodb-timescale:27017/")
+#     # Connect to the database and count the number of documents in a collection
+#     db = client["test_database"]
+#     coll = db["test_collection"]
+#     count = coll.count_documents({})
+
+#     print("MongoDB health check: OK")
+
+# except Exception as e:
+#     error_message = ("Error: Could not connect to MongoDB:", str(e))
+#     data = {
+#         "LOG_TYPE": "Alarm",
+#         "date": created_time,
+#         "layer_name": "KNOC",
+#         "error_message": error_message,
+#         "container": "Mongo-db",
+#     }
+#     producer.send("alarms", value=data)
+#     producer.flush()
