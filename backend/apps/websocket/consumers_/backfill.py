1779528910692463d4f3485fe7a8b9b77e0b9639
UmutBilgen <umut.reis.166@gmail.com>
2023-02-08 23:49:24 +0000

    Django: moved Backfill consumer from sync to async

---
+++ backend/apps/websocket/consumers_/backfill.py
@@ -0,0 +1,52 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import asyncio
+import json
+from pymongo import MongoClient, DESCENDING
+from asgiref.sync import sync_to_async,async_to_sync
+from utils.consumer_utils import find_tag,retrieve_backfill_data,createThread,delThread
+
+
+class WSConsumerBackfill(AsyncWebsocketConsumer):
+    
+    def send_messages(self,**kwargs):
+        pass
+       
+    async def connect(self):
+        await self.accept()
+        self.client = MongoClient("mongodb://root:admin@mongodb-timescale:27017/")
+        self.mongo_db = self.client["backfilldata3"]
+        self.collection = self.mongo_db["backfilldata3"]
+        self.tag_id = self.scope["url_route"]["kwargs"]["tag_id"]
+        self.tag_name,self.asset = await sync_to_async(find_tag)(self.tag_id)
+        self.kwargs = {
+            "query" : {
+                        "tag_name": self.tag_name,
+                        "asset": self.asset,
+                      },
+            "collection":self.collection}
+        qs = await sync_to_async(retrieve_backfill_data)(**self.kwargs)
+        await self.send(json.dumps(qs, ensure_ascii=False))
+    
+    async def receive(self, text_data):
+        try:
+            start_date, end_date = text_data.split(",")
+            query =  {
+                "$and": [
+                    {"tag_name": self.tag_name},
+                    {"asset": self.asset},
+                    {"date": {"$gte": start_date}},
+                    {"date": {"$lte": end_date}},
+                ]
+                }
+            self.kwargs["query"]=query
+            qs = await sync_to_async(retrieve_backfill_data)(**self.kwargs)
+            await self.send(json.dumps(qs, ensure_ascii=False))
+        except BaseException as e:
+            print(e)
+        
+    async def disconnect(self, close_code):
+        try:
+            self.client.close()
+            print("disconnect", close_code)
+        except BaseException as e:
+            print(e)
