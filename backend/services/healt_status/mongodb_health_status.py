9a892b69a3288025855cc9d005388413ff3c6e81
UmutBilgen <umut.reis.166@gmail.com>
2023-02-09 01:39:19 +0000

    DP: mongodb healt check refactor

--- backend/services/healt_status/mongodb_health_status.py
+++ backend/services/healt_status/mongodb_health_status.py
@@ -3,33 +3,50 @@ import os
 import datetime
 from kafka import KafkaProducer
 import json
+from helper import send_alarm
 
-host = os.environ.get("Kafka_Host_DP")
-created_time = datetime.datetime.now()
-client = MongoClient("mongodb://root:admin@mongodb-timescale:27017/")
-db = client["test_database"]
-coll = db["test_collection"]
 
 try:
+    client = MongoClient("mongodb://root:admin@mongodb-timescale:27017/")
+    db = client["test_database"]
+    coll = db["test_collection"]
     count = coll.count_documents({})
     print("MongoDB health check: OK")
 
 except Exception as e:
     error_message = f"Error: Could not connect to MongoDB: {e}"
-    data = {
-        "LOG_TYPE": "Alarm",
-        "date": created_time,
-        "layer_name": "KNOC",
-        "error_message": error_message,
-        "container": "Mongo-db",
-    }
-    producer = KafkaProducer(
-        bootstrap_servers=host,
-        value_serializer=lambda v: json.dumps(v).encode("ascii"),
-    )
-    producer.send("alarms", value=data)
-    producer.flush()
-    producer.close()
+    send_alarm(error_message,"Mongo-db")
+
+
+
+# HELPER FUNC BW
+# host = os.environ.get("Kafka_Host_DP")
+# created_time = datetime.datetime.now()
+# client = MongoClient("mongodb://root:admin@mongodb-timescale:27017/")
+# db = client["test_database"]
+# coll = db["test_collection"]
+
+# try:
+#     count = coll.count_documents({})
+#     print("MongoDB health check: OK")
+
+# except Exception as e:
+#     error_message = f"Error: Could not connect to MongoDB: {e}"
+#     data = {
+#         "LOG_TYPE": "Alarm",
+#         "date": created_time,
+#         "layer_name": "KNOC",
+#         "error_message": error_message,
+#         "container": "Mongo-db",
+#     }
+#     producer = KafkaProducer(
+#         bootstrap_servers=host,
+#         value_serializer=lambda v: json.dumps(v).encode("ascii"),
+#     )
+#     producer.send("alarms", value=data)
+#     producer.flush()
+#     producer.close()
+# **********************************************************************
 
 # from pymongo import MongoClient
 # import os
