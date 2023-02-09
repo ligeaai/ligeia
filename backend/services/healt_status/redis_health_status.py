0edb858a86ccfee44f6057a19cdcaeeac04eb26f
UmutBilgen <umut.reis.166@gmail.com>
2023-02-09 01:40:34 +0000

    DP: redis healt check refactor

--- backend/services/healt_status/redis_health_status.py
+++ backend/services/healt_status/redis_health_status.py
@@ -3,14 +3,7 @@ import os
 import datetime
 from kafka import KafkaProducer
 import json
-
-host = os.environ.get("Kafka_Host_DP")
-producer = KafkaProducer(
-    bootstrap_servers=host,
-    value_serializer=lambda v: json.dumps(v).encode("ascii"),
-)
-created_time = datetime.datetime.now()
-
+from helper import send_alarm
 
 def check_ts_redis():
     try:
@@ -19,15 +12,7 @@ def check_ts_redis():
         print("ok")
     except Exception as e:
         error_message = f"Redis health check failed: {e}"
-        data = {
-            "LOG_TYPE": "Alarm",
-            "date": created_time,
-            "layer_name": "KNOC",
-            "error_message": error_message,
-            "container": "Redis",
-        }
-        producer.send("alarms", value=data)
-        producer.flush()
+        send_alarm(error_message,"Redis-TS")
 
 
 def check_cache_redis():
@@ -37,16 +22,56 @@ def check_cache_redis():
         print("ok")
     except Exception as e:
         error_message = f"Redis health check failed: {e}"
-        data = {
-            "LOG_TYPE": "Alarm",
-            "date": created_time,
-            "layer_name": "KNOC",
-            "error_message": error_message,
-            "container": "Redis",
-        }
-        producer.send("alarms", value=data)
-        producer.flush()
+        send_alarm(error_message,"Redis")
 
 
 check_ts_redis()
 check_cache_redis()
+
+
+# host = os.environ.get("Kafka_Host_DP")
+# producer = KafkaProducer(
+#     bootstrap_servers=host,
+#     value_serializer=lambda v: json.dumps(v).encode("ascii"),
+# )
+# created_time = datetime.datetime.now()
+
+
+# def check_ts_redis():
+#     try:
+#         redis_conn = redis.StrictRedis("redis-test1", port=6379)
+#         redis_conn.ping()
+#         print("ok")
+#     except Exception as e:
+#         error_message = f"Redis health check failed: {e}"
+#         data = {
+#             "LOG_TYPE": "Alarm",
+#             "date": created_time,
+#             "layer_name": "KNOC",
+#             "error_message": error_message,
+#             "container": "Redis",
+#         }
+#         producer.send("alarms", value=data)
+#         producer.flush()
+
+
+# def check_cache_redis():
+#     try:
+#         redis_conn = redis.StrictRedis("ligeiaai-redis-1", port=6379)
+#         redis_conn.ping()
+#         print("ok")
+#     except Exception as e:
+#         error_message = f"Redis health check failed: {e}"
+#         data = {
+#             "LOG_TYPE": "Alarm",
+#             "date": created_time,
+#             "layer_name": "KNOC",
+#             "error_message": error_message,
+#             "container": "Redis",
+#         }
+#         producer.send("alarms", value=data)
+#         producer.flush()
+
+
+# check_ts_redis()
+# check_cache_redis()
