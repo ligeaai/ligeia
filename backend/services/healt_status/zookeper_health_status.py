a0ce1afdddf6e547f81b85c06b29b8d320428cd5
UmutBilgen <umut.reis.166@gmail.com>
2023-02-09 01:40:49 +0000

    DP: zookeper healt check refactor

--- backend/services/healt_status/zookeper_health_status.py
+++ backend/services/healt_status/zookeper_health_status.py
@@ -3,13 +3,7 @@ import os
 import datetime
 from kafka import KafkaProducer
 import json
-
-host = os.environ.get("Kafka_Host_DP")
-producer = KafkaProducer(
-    bootstrap_servers=host,
-    value_serializer=lambda v: json.dumps(v).encode("ascii"),
-)
-created_time = datetime.datetime.now()
+from helper import send_alarm
 
 try:
     zk = KazooClient(hosts="zookeeper:2181")
@@ -19,12 +13,30 @@ try:
     print("ok")
 except Exception as e:
     error_message = f"ZooKeeper health check failed: {e}"
-    data = {
-        "LOG_TYPE": "Alarm",
-        "date": created_time,
-        "layer_name": "KNOC",
-        "error_message": error_message,
-        "container": "Zookeeper",
-    }
-    producer.send("alarms", value=data)
-    producer.flush()
+    send_alarm(error_message,"Zookeeper")
+
+
+# host = os.environ.get("Kafka_Host_DP")
+# producer = KafkaProducer(
+#     bootstrap_servers=host,
+#     value_serializer=lambda v: json.dumps(v).encode("ascii"),
+# )
+# created_time = datetime.datetime.now()
+
+# try:
+#     zk = KazooClient(hosts="zookeeper:2181")
+#     zk.start()
+#     zk.ensure_path("/")
+#     zk.stop()
+#     print("ok")
+# except Exception as e:
+#     error_message = f"ZooKeeper health check failed: {e}"
+#     data = {
+#         "LOG_TYPE": "Alarm",
+#         "date": created_time,
+#         "layer_name": "KNOC",
+#         "error_message": error_message,
+#         "container": "Zookeeper",
+#     }
+#     producer.send("alarms", value=data)
+#     producer.flush()
