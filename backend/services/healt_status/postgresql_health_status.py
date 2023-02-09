44a51946622cf9768ab51cef6b4047c2c3b0f509
UmutBilgen <umut.reis.166@gmail.com>
2023-02-09 01:39:58 +0000

    DP: postgre healt check refactor

--- backend/services/healt_status/postgresql_health_status.py
+++ backend/services/healt_status/postgresql_health_status.py
@@ -3,12 +3,7 @@ import os
 import json
 from kafka import KafkaProducer
 from datetime import datetime
-
-host = os.environ.get("Kafka_Host_DP")
-producer = KafkaProducer(
-    bootstrap_servers=host,
-    value_serializer=lambda v: json.dumps(v).encode("ascii"),
-)
+from helper import send_alarm
 
 def HealthCheckForPostgre():
     try:
@@ -24,19 +19,47 @@ def HealthCheckForPostgre():
             print("PostgreSQL is not running.")
     except (psycopg2.OperationalError, psycopg2.DatabaseError) as e:
         error_message = ("Error connecting to PostgreSQL:", str(e))
-        data = {
-            "LOG_TYPE": "Alarm",
-            "date": str(datetime.now()),
-            "layer_name": "KNOC",
-            "error_message": error_message,
-            "container": "Postgre-SQL",
-        }
-        producer.send("alarms", value=data)
-        producer.flush()
+        send_alarm(error_message,"Postgre-SQL")
     finally:
         cur.close()
         conn.close()
     print("finally")
+
+
+# HELPER FUNC BW
+# host = os.environ.get("Kafka_Host_DP")
+# producer = KafkaProducer(
+#     bootstrap_servers=host,
+#     value_serializer=lambda v: json.dumps(v).encode("ascii"),
+# )
+
+# def HealthCheckForPostgre():
+#     try:
+#         conn = psycopg2.connect(
+#             host="postgres", database="postgres", user="postgres", password="manager"
+#         )
+#         cur = conn.cursor()
+#         cur.execute("SELECT 1")
+#         result = cur.fetchone()
+#         if result == (1,):
+#             print("PostgreSQL is up and running!")
+#         else:
+#             print("PostgreSQL is not running.")
+#     except (psycopg2.OperationalError, psycopg2.DatabaseError) as e:
+#         error_message = ("Error connecting to PostgreSQL:", str(e))
+#         data = {
+#             "LOG_TYPE": "Alarm",
+#             "date": str(datetime.now()),
+#             "layer_name": "KNOC",
+#             "error_message": error_message,
+#             "container": "Postgre-SQL",
+#         }
+#         producer.send("alarms", value=data)
+#         producer.flush()
+#     finally:
+#         cur.close()
+#         conn.close()
+#     print("finally")
 
 
 # import psycopg2
