4701509ac9650c0ccb2bf839d79fb48ae7596b96
UmutBilgen <umut.reis.166@gmail.com>
2023-02-09 01:40:16 +0000

    DP: rabitmq  healt check refactor

--- backend/services/healt_status/rabitmq_health_status.py
+++ backend/services/healt_status/rabitmq_health_status.py
@@ -3,12 +3,7 @@ import os
 import json
 from kafka import KafkaProducer
 from datetime import datetime
-
-host = os.environ.get("Kafka_Host_DP")
-producer = KafkaProducer(
-    bootstrap_servers=host,
-    value_serializer=lambda v: json.dumps(v).encode("ascii"),
-)
+from helper import send_alarm
 base_url = os.environ.get("BASE_URL")
 
 
@@ -23,15 +18,38 @@ def HealthCheckForRabbitMQ():
             raise Exception(f"RabbitMQ health check failed: {response.status_code}")
     except Exception as e:
         error_message = f"RabbitMQ health check failed: {e}"
-        data = {
-            "LOG_TYPE": "Alarm",
-            "date": str(datetime.now()),
-            "layer_name": "KNOC",
-            "error_message": error_message,
-            "container": "Rabbit-MQ",
-        }
-        producer.send("alarms", value=data)
-        producer.flush()
+        send_alarm(error_message,"Rabbit-MQ")
+
+
+# HELPER FUNC BW
+# host = os.environ.get("Kafka_Host_DP")
+# producer = KafkaProducer(
+#     bootstrap_servers=host,
+#     value_serializer=lambda v: json.dumps(v).encode("ascii"),
+# )
+# base_url = os.environ.get("BASE_URL")
+
+
+# def HealthCheckForRabbitMQ():
+#     try:
+#         response = requests.get(
+#             base_url + ":15672/api/aliveness-test/%2F", auth=("guest", "guest")
+#         )
+#         if response.status_code == 200 and response.json()["status"] == "ok":
+#             print("RabbitMQ is up and running!")
+#         else:
+#             raise Exception(f"RabbitMQ health check failed: {response.status_code}")
+#     except Exception as e:
+#         error_message = f"RabbitMQ health check failed: {e}"
+#         data = {
+#             "LOG_TYPE": "Alarm",
+#             "date": str(datetime.now()),
+#             "layer_name": "KNOC",
+#             "error_message": error_message,
+#             "container": "Rabbit-MQ",
+#         }
+#         producer.send("alarms", value=data)
+#         producer.flush()
 
 
 # import requests
