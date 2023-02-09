92d3782fb0b005b06336001deb1ff9e3b8fba033
UmutBilgen <umut.reis.166@gmail.com>
2023-02-09 01:39:06 +0000

    DP: kafka healt check refactor

--- backend/services/healt_status/kafka_health_status.py
+++ backend/services/healt_status/kafka_health_status.py
@@ -3,13 +3,9 @@ import os
 import datetime
 from kafka import KafkaProducer
 import json
-
+from helper import send_alarm
 host = os.environ["Kafka_Host_DP"]
-producer = KafkaProducer(
-    bootstrap_servers=host,
-    value_serializer=lambda v: json.dumps(v).encode("ascii"),
-)
-created_time = datetime.datetime.now()
+
 
 try:
     consumer = KafkaConsumer(
@@ -21,12 +17,33 @@ try:
     print("Ok")
 except Exception as e:
     error_message = f"Kafka health check failed: {e}"
-    data = {
-        "LOG_TYPE": "Alarm",
-        "date": created_time,
-        "layer_name": "KNOC",
-        "error_message": error_message,
-        "container": "Apache-kafka",
-    }
-    producer.send("alarms", value=data)
-    producer.flush()
+    send_alarm(error_message,"Apache-kafka")
+
+
+
+# host = os.environ["Kafka_Host_DP"]
+# producer = KafkaProducer(
+#     bootstrap_servers=host,
+#     value_serializer=lambda v: json.dumps(v).encode("ascii"),
+# )
+# created_time = datetime.datetime.now()
+
+# try:
+#     consumer = KafkaConsumer(
+#         bootstrap_servers=host,
+#         enable_auto_commit=False,
+#         auto_offset_reset="earliest",
+#     )
+#     consumer.topics()
+#     print("Ok")
+# except Exception as e:
+#     error_message = f"Kafka health check failed: {e}"
+#     data = {
+#         "LOG_TYPE": "Alarm",
+#         "date": created_time,
+#         "layer_name": "KNOC",
+#         "error_message": error_message,
+#         "container": "Apache-kafka",
+#     }
+#     producer.send("alarms", value=data)
+#     producer.flush()
