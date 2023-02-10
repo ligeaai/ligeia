eb53548fbd9bc81d6c2371ed04c7d773d7b168bb
UmutBilgen <umut.reis.166@gmail.com>
2023-02-10 23:17:43 +0000

    Django: Websocket refactor

--- backend/apps/websocket/consumers_/live_data.py
+++ backend/apps/websocket/consumers_/live_data.py
@@ -4,48 +4,40 @@ import asyncio
 import json
 import time
 import redis
-from asgiref.sync import sync_to_async,async_to_sync
-from utils.consumer_utils import find_tag,retive_live_data,delThread,createThread
+from asgiref.sync import sync_to_async, async_to_sync
+from utils.consumer_utils import find_tag, retive_live_data
+
 
 class WSLiveConsumer(AsyncWebsocketConsumer):
-    
-    def send_messages(self,**kwargs):
+    async def send_messages(self):
         while self.is_active:
-            query_tuple = tuple()
-            query_tuple = retive_live_data(**kwargs)
-            kwargs['start_time']=query_tuple[0]
-            kwargs['end_time']=query_tuple[1]
-            if len(query_tuple) > 2:
-                async_to_sync(self.send)(json.dumps(query_tuple[2], ensure_ascii=False))
-            asyncio.sleep(5)
+            query_tuple = retive_live_data(**self.kwargs)
+            self.kwargs["start_time"], self.kwargs["end_time"], *data = query_tuple
+            if data:
+                await self.send(json.dumps(data[0], ensure_ascii=False))
+            await asyncio.sleep(5)
 
-    
     async def connect(self):
         await self.accept()
         self.rds = redis.StrictRedis("redis-test1", port=6379)
         self.tag_id = self.scope["url_route"]["kwargs"]["tag_id"]
         self.is_active = True
-        tag_name,asset = await sync_to_async(find_tag)(self.tag_id)
-        self.kwargs = {
-            "tag_name":tag_name,
-            "asset":asset,
-            "redis":self.rds.ts()}
-        self.thread = createThread(self.send_messages,self.kwargs)
-    
+        tag_name, asset = await sync_to_async(find_tag)(self.tag_id)
+        self.kwargs = {"tag_name": tag_name, "asset": asset, "redis": self.rds.ts()}
+        self.task = asyncio.create_task(self.send_messages())
+
     async def receive(self, text_data):
         try:
-            self.is_active = False
-            delThread(self.thread)
-            self.kwargs["start_time"]= str(text_data.split(",")[0])
-            self.kwargs["end_time"]= str(text_data.split(",")[1])
-            self.thread = createThread(self.send_messages,self.kwargs)
+            start_time, end_time = text_data.split(",")
+            self.kwargs["start_time"] = start_time
+            self.kwargs["end_time"] = end_time
         except:
             pass
-        
+
     async def disconnect(self, close_code):
         try:
             self.is_active = False
-            delThread(self.thread)
+            self.task.cancel()
             self.rds.connection_pool.disconnect()
             print("disconnect", close_code)
         except BaseException as e:
