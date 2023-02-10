64c185013f31d36413e41c9056b6647808f37526
beisekin <mr.batkaval@gmail.com>
2023-02-10 17:08:02 +0600

    Django : refactor

--- backend/apps/uoms/urls.py
+++ backend/apps/uoms/urls.py
@@ -1,15 +1,10 @@
 from django.urls import include, path, re_path
 from django.urls.resolvers import URLPattern
 
-from .views import (UomSaveView,UOMScriptView,UomDetialsView)
+from .views import UomSaveView, UOMScriptView, UomDetialsView
 
 urlpatterns = [
-    
-    path("save/", UomSaveView.as_view(),name='UomSave'),
-
-    path("scripts/", UOMScriptView.as_view(),name='Scripts'),
-    
-    path("details/", UomDetialsView.as_view(),name='details'),
-
-    
-] 
+    path("save/", UomSaveView.as_view(), name="UomSave"),
+    path("scripts/", UOMScriptView.as_view(), name="Scripts"),
+    path("details/", UomDetialsView.as_view(), name="details"),
+]
