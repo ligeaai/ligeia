c126d7a4900391d0ebccb12a284280ea37af36a3
UmutBilgen <umut.reis.166@gmail.com>
2023-02-14 08:55:00 +0000

    Django: get uoms all filed end-point

--- backend/apps/uoms/urls.py
+++ backend/apps/uoms/urls.py
@@ -7,12 +7,14 @@ from .views import (
     UomDetialsView,
     UomEditorSaveUpdateView,
     UomDeleteView,
+    UomDetialsAllView,
 )
 
 urlpatterns = [
     path("save/", UomSaveView.as_view(), name="UomSave"),
     path("scripts/", UOMScriptView.as_view(), name="Scripts"),
     path("details/", UomDetialsView.as_view(), name="details"),
+    path("details/all", UomDetialsAllView.as_view(), name="details"),
     path("save-update/", UomEditorSaveUpdateView.as_view(), name="save-update"),
     path("delete/", UomDeleteView.as_view(), name="delete"),
 ]
