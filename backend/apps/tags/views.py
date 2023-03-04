bffe250b092262740443af5b74ed2d0341db3c7e
UmutBilgen <umut.reis.166@gmail.com>
2023-03-04 20:07:12 +0000

    django: TAG refactor

--- backend/apps/tags/views.py
+++ backend/apps/tags/views.py
@@ -12,7 +12,7 @@ from .serializers import (
 )
 from apps.type_property.models import type_property
 from apps.resource_list.models import resource_list
-from apps.resource_list.serializers import ResourceListSerializer
+from apps.resource_list.serializers import ResourceListLabelSerializer
 from apps.type_property.serializers import TypePropertyDetailsSerializer
 from apps.item_link.models import item_link
 from apps.item_link.serializers import ItemLinkSaveSerializer, ItemLinkDetailsSerializer
@@ -24,6 +24,7 @@ from apps.templates.orm_CodeList import CodeListORM
 from apps.type_link.serializers import TypeLinkDetailsSerializer
 from utils.models_utils import validate_model_not_null
 import datetime
+from django.db import transaction
 
 # Create your views here.
 
@@ -44,34 +45,35 @@ class TagsSaveView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
-        tags_dict = request.data
-        link_dict = dict()
-        try:
-            links_list = [
-                "LINK_ID",
-                "LINK_TYPE",
-                "END_DATETIME",
-                "FROM_ITEM_ID",
-                "FROM_ITEM_TYPE",
-                "TO_ITEM_ID",
-                "TO_ITEM_TYPE",
-            ]
-            for keys in links_list:
-                link_dict[keys] = request.data.get(keys)
-                tags_dict.pop(keys)
-        except:
-            pass
-        link_dict["START_DATETIME"] = tags_dict.get("START_DATETIME")
-        link_dict["ROW_ID"] = uuid.uuid4().hex
-        validate_model_not_null(tags_dict, "tags", request=request)
-        validate_model_not_null(link_dict, "ITEM_LINK", request=request)
-        serializer = TagsSaveSerializer(data=request)
-        serializer.is_valid()
-        message = serializer.save(tags_dict)
-        link_serializer = ItemLinkSaveSerializer(data=request.data.get("LINK"))
-        link_serializer.is_valid()
-        message = link_serializer.save(link_dict)
-        return Response(message, status=status.HTTP_200_OK)
+        with transaction.atomic():
+            tags_dict = request.data
+            link_dict = {
+                "LINK_ID": tags_dict.get("LINK_ID"),
+                "TO_ITEM_ID": tags_dict.get("ITEM_ID"),
+                "TO_ITEM_TYPE": tags_dict.get("TRANSACTION_TYPE"),
+                "END_DATETIME": "9000-01-01",
+                "FROM_ITEM_ID": tags_dict.get("TAG_ID"),
+                "FROM_ITEM_TYPE": "TAG_CACHE",
+                "LINK_TYPE": "TAG_ITEM",
+                "START_DATETIME": tags_dict.get("START_DATETIME"),
+                "ROW_ID": uuid.uuid4().hex,
+                # "LAST_UPDT_USER":request.user
+            }
+            del tags_dict["LINK_ID"]
+            validate_model_not_null(tags_dict, "tags", request=request)
+            serializer = TagsSaveSerializer(data=tags_dict)
+            serializer.is_valid()
+            message = serializer.save(tags_dict)
+            try:
+                validate_model_not_null(link_dict, "ITEM_LINK", request=request)
+                link_serializer = ItemLinkSaveSerializer(data=request.data.get("LINK"))
+                link_serializer.is_valid()
+                message = link_serializer.save(link_dict)
+            except Exception as e:
+                transaction.set_rollback(True)
+                return Response({"error": e}, status=status.HTTP_400_BAD_REQUEST)
+
+            return Response(message, status=status.HTTP_200_OK)
 
 
 class TagsDetailsView(generics.ListAPIView):
@@ -82,10 +84,9 @@ class TagsDetailsView(generics.ListAPIView):
         pass
 
     def get(self, request, *args, **kwargs):
-        queryset = tags.objects.all()
+        queryset = tags.objects.all().order_by("NAME").values()
         serializer = TagsDetiailsSerializer(queryset, many=True)
-        sorted_list = sorted(list(serializer.data), key=lambda d: str(d["NAME"]))
-        return Response(sorted_list, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class TagsSpesificDetailsView(generics.CreateAPIView):
@@ -120,18 +121,27 @@ class TagsPropertysView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
-        queryset = type_property.objects.filter(TYPE="TAG_INFO")
-        serializer = TypePropertyDetailsSerializer(queryset, many=True)
-        queryset_link = type_property.objects.filter(TYPE="TAG_LINK")
-        serializer_link = TypePropertyDetailsSerializer(queryset_link, many=True)
+        tag_info = type_property.objects.filter(TYPE="TAG_INFO")
+        tag_link = type_property.objects.filter(TYPE="TAG_LINK")
+        serializer_info = TypePropertyDetailsSerializer(tag_info, many=True).data
+        serializer_link = TypePropertyDetailsSerializer(tag_link, many=True).data
         tag_INFO = []
         tag_LINK = []
-        self._resourceLabel(serializer.data, tag_INFO, request.data.get("CULTURE"))
-        self._resourceLabel(serializer_link.data, tag_LINK, request.data.get("CULTURE"))
+        self._resourceLabel(serializer_info, tag_INFO, request.data.get("CULTURE"))
+        self._resourceLabel(serializer_link, tag_LINK, request.data.get("CULTURE"))
         new_dict = {"TAG_INFORMATIONS": tag_INFO, "TAG_LINK": tag_LINK}
         return Response(new_dict, status=status.HTTP_200_OK)
 
     def _resourceLabel(self, data, dataList, culture):
+        label_ids = [item.get("LABEL_ID") for item in data]
+        qs_resources = resource_list.objects.filter(ID__in=label_ids, CULTURE=culture)
+        resources = {}
+        for resource in qs_resources:
+            resources[resource.ID] = {
+                "SHORT_LABEL": resource.SHORT_LABEL,
+                "MOBILE_LABEL": resource.MOBILE_LABEL,
+            }
+
         for item in data:
             if item.get("CODE_LIST"):
                 qs_codeList = code_list.objects.filter(
@@ -141,14 +151,10 @@ class TagsPropertysView(generics.CreateAPIView):
                     qs_codeList, culture=culture, hierarchy=False
                 )
 
-            qs_resource = resource_list.objects.filter(
-                ID=item.get("LABEL_ID"), CULTURE=culture
-            )
-
-            if qs_resource:
-                serializer_rs = ResourceListSerializer(qs_resource, many=True)
-                item["SHORT_LABEL"] = serializer_rs.data[0].get("SHORT_LABEL")
-                item["MOBILE_LABEL"] = serializer_rs.data[0].get("MOBILE_LABEL")
+            resource = resources.get(item.get("LABEL_ID"))
+            if resource:
+                item["SHORT_LABEL"] = resource.get("SHORT_LABEL")
+                item["MOBILE_LABEL"] = resource.get("MOBILE_LABEL")
             dataList.append(item)
 
 
@@ -159,29 +165,28 @@ class TagsTypeLinkView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
-        queryset = type_link.objects.filter(TYPE="TAGS")
-        serializer = TypeLinkDetailsSerializer(queryset, many=True)
-        self.get_TypeName(serializer.data, request.data.get("CULTURE"))
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        culture = request.data.get("CULTURE")
+        to_type = list(
+            type_link.objects.filter(TYPE="TAGS")
+            .values_list("TO_TYPE", flat=True)
+            .order_by("TO_TYPE")
+        )
 
-    def get_TypeName(self, data, culture):
-        for index in range(len(data)):
-            try:
-                qs = type_model.objects.filter(TYPE=data[index].get("TO_TYPE"))
-                serializer = TypeResourceListManagerSerializer(qs, many=True)
-                label_id = serializer.data[0].get("LABEL_ID")
-                qs_resource = resource_list.objects.filter(ID=label_id, CULTURE=culture)
-                if qs_resource:
-                    serializer_rs = ResourceListSerializer(qs_resource, many=True)
-                    data[index]["SHORT_LABEL"] = serializer_rs.data[0].get(
-                        "SHORT_LABEL"
-                    )
-                    data[index]["MOBILE_LABEL"] = serializer_rs.data[0].get(
-                        "MOBILE_LABEL"
-                    )
-            except:
-                qs = type_model.objects.filter(TYPE=data[index].get("TO_TYPE"))
-                print(data[index].get("TO_TYPE"))
+        query = (
+            type_model.objects.filter(TYPE__in=to_type)
+            .values_list("LABEL_ID", flat=True)
+            .order_by("TYPE")
+        )
+
+        qs_resource = resource_list.objects.filter(
+            ID__in=query, CULTURE=culture
+        ).order_by("ID")
+
+        labels = ResourceListLabelSerializer(qs_resource, many=True).data
+        response_value = [
+            {**label, "TO_TYPE": to_type} for label, to_type in zip(labels, to_type)
+        ]
+        return Response(response_value, status=status.HTTP_200_OK)
 
 
 class TagsNameViews(generics.CreateAPIView):
@@ -214,7 +219,6 @@ class TagsUomConversionView(generics.ListAPIView):
         pass
 
     def get(self, request, *args, **kwargs):
-        queryset = tags.objects.all()
+        queryset = tags.objects.all().order_by("NAME").values()
         serializer = TagsUomConversionSerializer(queryset, many=True)
-        sorted_list = sorted(list(serializer.data), key=lambda d: str(d["NAME"]))
-        return Response(sorted_list, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
