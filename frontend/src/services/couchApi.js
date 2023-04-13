c4d7e62d26b097c0fd2ee37fdad0ff55ce7c6a02
EmirAlpAksungur <77407633+EmirAlpAksungur@users.noreply.github.com>
2023-04-14 00:26:27 +0300

    React : new api

--- frontend/src/services/couchApi.js
+++ frontend/src/services/couchApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'http://16.170.110.157:5984/'
+    baseURL: 'http://20.230.239.209:5984/'
 });
 export const userName = "COUCHDB_USER"
 export const userPassword = "COUCHDB_PASSWORD"
