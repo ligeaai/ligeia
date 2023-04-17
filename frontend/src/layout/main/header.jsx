4c76e315de9b9c9ad02063b4d92441783a59b0fe
EmirAlpAksungur <77407633+EmirAlpAksungur@users.noreply.github.com>
2023-04-17 23:00:43 +0300

    React: header =>scss

--- frontend/src/layout/main/header.jsx
+++ frontend/src/layout/main/header.jsx
@@ -1,31 +1,29 @@
 import * as React from "react";
 import $ from "jquery";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
-import { Grid, IconButton } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { Grid, IconButton, Box } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import MenuOpenOutlinedIcon from "@mui/icons-material/MenuOpenOutlined";
 
 import AlertIcon from "./alert/alarmsIcon";
 import SettingMenu from "./settingsMenu";
-import logo from "../../assets/Images/header/ligeiaLogo.png";
-import logoDark from "../../assets/Images/header/ligeiaLogoDark.png";
 import history from "../../routers/history";
 import { setOpenTab } from "../../services/actions/drawerMenu/drawerMenu";
+
 const DrawerIcon = () => {
   const dispatch = useDispatch();
   function handleClick() {
     $(".drawer-menu").toggleClass("drawer-menu-closed");
     $(".drawer-menu .drawer-menu__list-item__text").toggle(200);
-    $("#drawer-menu-icon-open").toggle(200);
-    $(".drawer-menu-icon-close").toggle(200);
+    $("#drawer-menu-icon-open").toggle();
+    $(".drawer-menu-icon-close").toggle();
     dispatch(setOpenTab("Drawer"));
   }
   return (
     <Grid item className="drawer-menu-icon-box">
       <IconButton onClick={handleClick}>
-        <MenuIcon id="drawer-menu-icon-open" className="drawer-menu-icon " />
+        <MenuIcon id="drawer-menu-icon-open" className="drawer-menu-icon" />
         <MenuOpenOutlinedIcon className="drawer-menu-icon drawer-menu-icon-close" />
       </IconButton>
     </Grid>
@@ -33,66 +31,29 @@ const DrawerIcon = () => {
 };
 
 const Header = () => {
-  const theme = useSelector((state) => state.theme.theme);
-  const search = useSelector((state) => state.searchBar.isFocus);
-  const MyBox = styled(Grid)(({ theme }) => {
-    console.log(theme);
-    return {
-      backgroundColor: theme.palette.primary.dark, // this
-      alignItems: "center",
-      justifyContent: "space-between",
-      padding: "13px 21px",
-      height: "59px",
-    };
-  });
-
   return (
-    <React.Fragment>
-      <MyBox
-        container
-        sx={{
-          display: {
-            xs: search ? "flex" : "none",
-            md: "none",
-          },
-        }}
-      ></MyBox>
-      <MyBox
-        sx={{
-          display: {
-            xs: search ? "none" : "flex",
-            md: "flex",
-          },
-        }}
-      >
-        <Grid item>
-          <Grid container spacing={1.5} alignItems="center">
-            <Grid item>
-              <DrawerIcon />
-            </Grid>
-            <Grid
-              item
-              sx={{ cursor: "pointer" }}
+    <Grid container className="app-header">
+      <Grid item className="app-header-box">
+        <Grid container className="app-header__left-box">
+          <DrawerIcon />
+          <Grid item className="app-header__left-box__divider" />
+          <Grid item>
+            <Box
+              className="app-header__left-box__app-logo"
               onClick={() => {
                 history.push("/");
               }}
-            >
-              {theme.includes("Dark") || theme.includes("dark") ? (
-                <img src={logoDark} alt="logo" width={100} />
-              ) : (
-                <img src={logo} alt="logo" width={100} />
-              )}
-            </Grid>
+            />
           </Grid>
         </Grid>
-        <Grid item>
-          <Grid container columnSpacing={2} sx={{ alignItems: "center" }}>
-            <AlertIcon />
-            <SettingMenu />
-          </Grid>
+      </Grid>
+      <Grid itemclassName="app-header-box">
+        <Grid container columnSpacing={2} className="app-header__right-box">
+          <AlertIcon />
+          <SettingMenu />
         </Grid>
-      </MyBox>
-    </React.Fragment>
+      </Grid>
+    </Grid>
   );
 };
 
