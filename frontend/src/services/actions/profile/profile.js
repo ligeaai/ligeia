12141db3d70f7f72ea062e09dc51a1c7f59b4a90
EmirAlpAksungur <77407633+EmirAlpAksungur@users.noreply.github.com>
2023-04-25 22:49:37 +0300

    React:fix

--- frontend/src/services/actions/profile/profile.js
+++ frontend/src/services/actions/profile/profile.js
@@ -3,18 +3,29 @@ import {
     CLEAN_PROFILE
 } from "../types"
 import Auth from "../../api/auth"
+import { loadUser } from "../auth"
 export const loadProfile = () => async (dispatch) => {
     try {
         let res = await Auth.get()
-        console.log(res.data);
         dispatch({
             type: LOAD_PROFILE,
             payload: res.data
         })
     } catch {
-
     }
 }
+
+export const updateUserInfo = (user) => async (dispatch) => {
+    try {
+        let res = await Auth.profileUpdate(user)
+        console.log(res);
+        dispatch(loadProfile())
+        dispatch(loadUser())
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 export const cleanProfile = () => disppatch => {
     disppatch({
         type: CLEAN_PROFILE,
@@ -22,4 +33,11 @@ export const cleanProfile = () => disppatch => {
     })
 }
 
+export const changePassword = async (body) => {
+    try {
+        let res = await Auth.updatePassword(body)
 
+    } catch (err) {
+        console.log(err);
+    }
+}
